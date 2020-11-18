e888ea816bea0db21eae3a8e9101b731d1a2fa40
banti386 <63452850+banti386@users.noreply.github.com>
2020-11-18 15:10:04 +0530

    Update providers.tf

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
   access_key = "AKIAXVP64B6LFTGEIOR5"
-  secret_key = "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"
+  secret_key = "WzlJaOAV8pxVFRLzlF8lAnncSqSn5XWuihE6+"
 }
