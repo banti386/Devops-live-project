37cc179d3d5a2c9c0bf40bf95a219be2ba1582d5
banti386 <63452850+banti386@users.noreply.github.com>
2020-12-01 18:21:40 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "myec2" {
-   count = "1"
-   ami = "ami-02354e95b39ca8dec"
+   ami = "ami-0affd4508a5d2481b"
    instance_type = "t2.nano"
    key_name = "terraform"
    tags          = {
@@ -8,16 +7,16 @@ resource "aws_instance" "myec2" {
 }
    provisioner "remote-exec" {
      inline = [
-       "sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm",
+       "sudo yum -y install epel*",
        "sudo yum install ansible -y",
        "sudo yum install docker -y",
        "sudo service docker start"
-        
+
      ]
 
    connection {
      type = "ssh"
-     user = "ec2-user"
+     user = "centos"
      private_key = file("./terraform.pem")
      host = self.public_ip
    }
@@ -27,26 +26,26 @@ resource "aws_instance" "myec2" {
 # docker-host
 
 resource "aws_instance" "myec3" {
-   count = "1"
-   ami = "ami-02354e95b39ca8dec"
+   ami = "ami-0affd4508a5d2481b"
    instance_type = "t2.nano"
    key_name = "terraform"
    tags          = {
      Name        = "Docker-host"
 }
-   
+
    provisioner "remote-exec" {
      inline = [
        "sudo yum install docker -y",
        "sudo service docker start"
-        
+
      ]
 
    connection {
      type = "ssh"
-     user = "ec2-user"
+     user = "centos"
      private_key = file("./terraform.pem")
      host = self.public_ip
    }
 }
    }
+
